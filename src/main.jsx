00629ee4cb8adfc5338331c600041bee547f8640
Tony <96173150+toonep06@users.noreply.github.com>
2024-07-11 10:25:20 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,44 +1,29 @@
 import React from 'react';
 import * as ReactDOM from 'react-dom/client';
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import App from './App.jsx';
 import Dashboard from './pages/DashboardPage.jsx';
 import './index.css';
 import LoginPage from './pages/LoginPage.jsx';
-import ProtectedRoute from './pages/ProtectedRoute.jsx'
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <LoginPage />,
-  },
-  {
-    path: "/login",
-    element: <LoginPage />,
-  },
-  {
-    path: "/home",
-    element: (
-      <ProtectedRoute>
-        <App />
-      </ProtectedRoute>
-    ),
-  },
-  {
-    path: "/dashboard",
-    element: (
-      <ProtectedRoute>
-        <Dashboard />
-      </ProtectedRoute>
-    ),
-  },
-]);
+import ProtectedRoute from './pages/ProtectedRoute.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/home" element={
+          <ProtectedRoute>
+            <App />
+          </ProtectedRoute>
+        } />
+        <Route path="/dashboard" element={
+          <ProtectedRoute>
+            <Dashboard />
+          </ProtectedRoute>
+        } />
+      </Routes>
+    </HashRouter>
   </React.StrictMode>,
 );
