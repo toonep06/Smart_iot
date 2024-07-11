8ea1323d8bedaf46850483223f5e1adbf6362d71
Tony <96173150+toonep06@users.noreply.github.com>
2024-07-11 10:20:06 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Smart_iot/login",
+  base: "/Smart_iot/",
   server: {
     proxy: {
       '/api': {
